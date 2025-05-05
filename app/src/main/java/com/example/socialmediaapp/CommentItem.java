622e091469e77c41ec5fa0e38f1954b785506758
tuan-cre <tuan_dth225757@student.agu.edu.vn>
2025-05-05 15:21:39 +0700

    comment Interface

---
+++ app/src/main/java/com/example/socialmediaapp/CommentItem.java
@@ -0,0 +1,8 @@
+package com.example.socialmediaapp;
+
+public class CommentItem {
+    private int commentId;
+    private String commenterName;
+    private String commentContent;
+    private String dateCreated;
+}
